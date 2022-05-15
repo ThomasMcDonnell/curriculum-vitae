569ae8589b7b3ec1f4aace8d4d6a33c38fe4fd61
Thomas McDonnell <thomas.mcdonnell@swrve.com>
2022-05-15 20:19:00 +0100

    production build

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content:[
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: [],
+}
